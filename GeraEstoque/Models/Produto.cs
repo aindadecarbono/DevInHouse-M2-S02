5ff486bf0f9e8f223d67e60516bf5ccd0c8d885e
Bruno Coelho <brunocoelhonet@gmail.com>
2022-06-22 18:04:28 -0300

    Ex6-S3

--- GeraEstoque/Models/Produto.cs
+++ GeraEstoque/Models/Produto.cs
@@ -2,5 +2,32 @@ namespace GeraEstoque.Models;
 
 public class Produto
 {
+  string NomeProduto;
+  ushort QuantidadeEstoque;
+  uint ValorCompra;
+  uint ValorVenda;
+  Guid Id;
+
+  public Produto()
+  {
+
+  }
+
+  public Produto(string nomeProduto, ushort quantidadeEstoque, uint valorCompra, uint valorVenda)
+  {
+
+    NomeProduto = nomeProduto;
+    QuantidadeEstoque = quantidadeEstoque;
+    ValorCompra = valorCompra;
+    ValorVenda = valorVenda;
+    Id = Guid.NewGuid();
+
+  }
+
+  public override string ToString()
+  {
+    return "Produto: Id: XXXX |  Nome: XXX | Qtd: XXX | R$ Compra: XXX | R$ Venda: XXX";
+  }
+
 
 }
